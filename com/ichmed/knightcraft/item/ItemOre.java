683e15e7bf566c40b712e611414083aa95a504a7
Ichmed <mijomeyer@t-online.de>
2014-01-22 00:46:17 +0100

    some new textures

--- com/ichmed/knightcraft/item/ItemOre.java
+++ com/ichmed/knightcraft/item/ItemOre.java
@@ -48,7 +48,8 @@ public class ItemOre extends Item
 		icons = new Icon[names.length];
 		for (int i = 0; i < names.length; i++)
 		{
-			icons[i] = iconRegister.registerIcon("knightcraft:" + type + names[i]);
+//			icons[i] = iconRegister.registerIcon("knightcraft:" + type + names[i]);
+			icons[i] = iconRegister.registerIcon("knightcraft:swordOrichalcum");
 		}
 	}
 
